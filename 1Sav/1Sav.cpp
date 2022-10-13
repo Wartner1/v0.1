cc2f6fe76323684eb2868dec389fba96cd0a4166
Sokol <Sokol@DESKTOP-LA3DS9J>
2022-10-13 21:04:42 +0300

    Add project files.

---
+++ 1Sav/1Sav.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+class Stud
+{
+public:
+	string vardas;
+	string pavarde;
+	int n_nd;
+	int egz;
+
+	double galutinis() {
+		return 0.4*
+	}
+};
+
+int main()
+{
+   
+
+}
